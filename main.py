7f494269b61164a29271d584b15bdc2bc419404b
drewfullerton <drewdfullerton@gmail.com>
2026-07-11 16:51:00 -0500

    It still only accepts a single prompt, but can now fix a code problem in the calculator app.

--- main.py
+++ main.py
@@ -17,8 +17,6 @@ client = OpenAI(
 )
 
 def main():
-    print("Hello from faregoai!")
-    
     #This is how we get a user prompt. If you add --verbose as an argument, it provides additional information
     parser = argparse.ArgumentParser(description="FaregoAI Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
@@ -26,25 +24,31 @@ def main():
     args = parser.parse_args()
     
     messages = [{"role": "system", "content": system_prompt}, {"role": "user","content": args.user_prompt,}]
-    response = client.chat.completions.create(model = 'openrouter/free', messages = messages, tools = available_functions, temperature = 0)
+    for _ in range(20):
+        response = client.chat.completions.create(model = 'openrouter/free', messages = messages, tools = available_functions, temperature = 0)
+        message = response.choices[0].message
+        messages.append(message)
     
-    #Print all the things (either the text response or the function calls it wants to make)!
-    if response.usage == None:
-        raise RuntimeError('Failed API Request')
-    else:
-        if args.verbose:
-            print(f'User prompt: {args.user_prompt}')
-            print(f'Prompt tokens: {response.usage.prompt_tokens}')
-            print(f'Response tokens: {response.usage.completion_tokens}')
-        if response.choices[0].message.tool_calls:
-            for tool_call in response.choices[0].message.tool_calls:
-                result_message = call_function(tool_call, args.verbose)
-                if result_message["content"] == None:
-                    raise Exception('Content is empty')
-                else:
-                    print(f"-> {result_message['content']}")
+        #Print all the things (either the text response or the function calls it wants to make)!
+        if response.usage == None:
+            raise RuntimeError('Failed API Request')
         else:
-            print(f'Response: {response.choices[0].message.content}')
-
+            if args.verbose:
+                print(f'User prompt: {args.user_prompt}')
+                print(f'Prompt tokens: {response.usage.prompt_tokens}')
+                print(f'Response tokens: {response.usage.completion_tokens}')
+            if response.choices[0].message.tool_calls:
+                for tool_call in response.choices[0].message.tool_calls:
+                    result_message = call_function(tool_call, args.verbose)
+                    messages.append(result_message)
+                    if result_message["content"] == None:
+                        raise Exception('Content is empty')
+                    #else:
+                        #print(f"-> {result_message['content']}")
+            else:
+                print(f'Response: {response.choices[0].message.content}')
+                exit(0)
+    print('Maximum number of iterations reached. Program exited.')
+    exit(1)
 if __name__ == "__main__":
     main()
